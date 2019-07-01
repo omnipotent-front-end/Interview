5b4d81e732eed2e9b661083604b4f9f29f3b80e4
刘放 <362512489@qq.com>
2019-07-01 17:37:59 +0800

    chore: init interview readme

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  title: "无所不能的前端--面试篇",
+  description: "Hello world",
+  themeConfig: {
+    sidebarDepth: 2,
+    sidebar: {
+      "": [
+        {
+          title: "编程语言",
+          children: [
+              "/language/html",
+              "/language/node"
+            ]
+        },
+        {
+            title: "库",
+            children: ["/library/vue.md"]
+          }
+      ]
+    }
+  }
+};
